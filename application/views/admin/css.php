24d7ba5973cf43a4dced2c1fc8abe0ae00b4ec26
yofandi <yofandirikiwinata24@gmail.com>
2018-01-16 15:11:08 +0700

    admin
    ui template

---
+++ application/views/admin/css.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/theme-floyd.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/theme-helper.css">
---
+++ application/views/admin/nav.php
@@ -0,0 +1,33 @@
+<div id="top-nav">
+				<nav class="navbar navbar-default">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<!-- Navbar toggle button -->
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
+								<i class="fa fa-bars"></i>
+							</button>
+							<!-- Sidebar toggle button -->
+							<button type="button" class="sidebar-toggle">
+								<i class="fa fa-bars"></i>
+							</button>
+							<a class="navbar-brand text-size-24" href="#"><i class="fa fa-star-o"></i> Welcome , Officer</a>
+						</div>
+						<div class="collapse navbar-collapse" id="menu">
+							<ul class="nav navbar-nav navbar-right">
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+										<span class="fa-stack">
+											<i class="fa fa-circle fa-stack-2x"></i>
+											<i class="fa fa-user fa-stack-1x"></i>
+										</span>
+									</a>
+									<ul class="dropdown-menu">
+										<li><a href="#"><i class="fa fa-fw fa-user"></i> Info</a></li>
+										<li><a href="#"><i class="fa fa-fw fa-power-off"></i> Logout</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
