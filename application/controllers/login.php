1eec8bcbcc1933299ce7f8a45a8ef05121d8f4d9
Unknown <dikaayik@gmail.com>
2018-01-16 00:43:16 -0800

    Merge branch 'ui'
    login page

---
+++ application/controllers/login.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('session');
+	}
+	public function index()
+	{
+		$this->load->view('login/index');		
+	}
+
+}
+
+/* End of file login.php */
+/* Location: ./application/controllers/login.php */
